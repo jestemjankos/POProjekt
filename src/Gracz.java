9030e9c5b637dd4ed64959f932b45e9832e19a19
ElWalach <leonwalach653@gmail.com>
2024-12-31 22:38:38 +0100

    klasa gracz
    dodanie klasy gracz; obiekt przechowuje imię i saldo gracza, klasa importuje interfejs Serializable

---
+++ src/Gracz.java
@@ -0,0 +1,41 @@
+import java.io.Serializable;
+
+/**
+ * importujemy Serializable żeby można było
+ * zapisywać i odczytywać obiekty tej klasy
+ * do/z pliku binarnego
+ */
+public class Gracz implements Serializable {
+    private String imie;
+    private int saldo;
+
+    /**
+     * @param imie the imie
+     * @param saldo the imie
+     */
+    public Gracz(String imie, int saldo) {
+
+        this.imie = imie;
+        this.saldo = saldo;
+    }
+
+
+    public String getImie() {
+        return imie;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo( int saldo ){
+        this.saldo= saldo;
+    }
+    public void dodajSaldo( int kwota ){
+        saldo+= kwota;
+    }
+
+    public void odejmijSaldo(int kwota){
+        saldo-= kwota;
+    }
+}
